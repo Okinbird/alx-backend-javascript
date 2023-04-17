d3fd8d022a1d3c6bd0bdfd9641b0be01918a8534
Okinbird <okinyinka@yahoo.co.uk>
2023-04-17 15:23:20 -0700

    ES6 for...of iteration of arrays value

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const newValue = [];
   for (let value of array) {
     value = appendString + value;
-    newValue.push(value);  
+    newValue.push(value);
   }
 
   return newValue;
