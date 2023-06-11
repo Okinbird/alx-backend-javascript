30135a158b766aa7fca25bda71ef3b0a52c4ba0c
Okinbird <okinyinka@yahoo.co.uk>
2023-06-11 03:18:11 -0700

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
   let chunk;
   while ((chunk = process.stdin.read()) !== null) {
-   process.stdout.write(`Your name is: ${chunk}`);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write("This important software is now closing\n");
+  process.stdout.write('This important software is now closing\n');
 });
